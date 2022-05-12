63ca85ca091405c78269a74f10c8c7d2e78906f6
Anthony <tonyudeagbala@gmail.com>
2022-05-12 16:41:50 +0100

    Make general imports

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
